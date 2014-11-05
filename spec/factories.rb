782b6e1ff91d61fb36a0911ccc5ae52c567377aa
speed lead <xuanyongyuliu@gmail.com>
2014-11-05 16:47:01 +0800

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,15 @@
 FactoryGirl.define do
   factory :user do
-    name "Speed Lead"
-    email "sl@example.com"
+    sequence(:name){|n| "Person #{n}"}
+    sequence(:email){|n| "pserson_#{n}@example.com"}
     password "foobar"
     password_confirmation "foobar"
+    # name "Speed Lead"
+    # email "sl@example.com"
+    # password "foobar"
+    # password_confirmation "foobar"
+    factory :admin do
+      admin true
+    end
   end
 end
